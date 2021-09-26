f5afb0045d4154834ca34234c8fd0f3eb0a094d6
Matjaž Godec <matjaz.godec@gmail.com>
2021-09-27 00:07:01 +0200

    testing script On branch ecoventv2 Your branch is up to date with 'origin/ecoventv2'.
    Changes to be committed:
    	new file:   test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+from ecoventv2 import Fan
+
+fan=Fan("10.94.0.106","003A00345842570A","1111")
+print(fan.state)
+print(fan.speed)
+print(fan.man_speed)
+print(fan.airflow)
+print(fan.humidity)
+#
+#fan.set_speed(2)
+#print(fan.speed)
+#fan.set_state_off()
+#fan.set_state_on()
+
