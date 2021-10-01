b0922b30d96bfd4d98ba21c83469c7665cd716b0
Matjaž Godec <matjaz.godec@gmail.com>
2021-10-02 01:49:38 +0200

    ...  Changes to be committed: 	modified:   ../test.py

--- test.py
+++ test.py
@@ -2,7 +2,16 @@ from ecoventv2 import Fan
 
 fan=Fan("10.94.0.106","003A00345842570A","1111")
 
-#print("TUKAJ:")
+
+
+print("TUKAJ:")
+# fan.read_param("000b")
+fan.read_param("0024")
+fan.read_param("0302")
+fan.read_param("0303")
+fan.read_param("0304")
+fan.read_param("002403020303030400a3")
+
 #print(fan.state)
 #print(fan.speed)
 #print(fan.man_speed)
