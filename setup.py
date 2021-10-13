78d81cf95c9d469a88a71e395eaf983b0f64633d
Matjaž Godec <matjaz.godec@gmail.com>
2021-10-13 23:13:56 +0200

    Reverted local changes after merge of PR
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   README.md
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", 'r') as fp:
     long_description = fp.read()
 
 setup(
-    name = 'pyEcoventV2',
+    name = 'pyEcovent',
     packages = ['ecovent','ecoventv2'],
-    version='0.9.1',
-    description='Python3 library for single-room energy recovery ventilators with api V2 from Vents / Blauberg / Flexit',
+    version='0.9.0',
+    description='Python3 library for single-room energy recovery ventilators from Vents / Blauberg / Flexit',
     long_description=long_description,
     python_requires='>=3.6.7',
-    author='Matjaž Godec/Aleksander Lehmann',
-    author_email='matjaz.godec@gmail.com,aleksander@flovik.no',
-    url='https://github.com/gody01/pyEcoventV2',
+    author='Aleksander Lehmann',
+    author_email='aleksander@flovik.no',
+    url='https://github.com/aglehmann/pyEcovent',
     license="MIT",
     classifiers=[
         'Intended Audience :: Developers',
